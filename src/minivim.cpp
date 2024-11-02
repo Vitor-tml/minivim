0b22c262676469f21f66d4b1e8d39939dcb8d378
vitor-tml <vitor.gabriel.vinielle@gmail.com>
2024-11-02 16:21:21 -0300

    Lógica inicial implementada

--- src/minivim.cpp
+++ src/minivim.cpp
@@ -7,6 +7,9 @@ Minivim::Minivim(std::string &filename){
     cbreak();
     keypad(stdscr, true);
 
+    x = y = 0;
+    mode = 'n';
+    status = "NORMAL";
     gArquivos = FileManager::getInstance();
     if(filename.empty())
     {
@@ -15,7 +18,8 @@ Minivim::Minivim(std::string &filename){
     
     gArquivos->loadFile(filename);
     file = gArquivos->getFile(filename);
-    
+
+    lines.push_back("");
 }
 
 Minivim::~Minivim()
@@ -25,6 +29,71 @@ Minivim::~Minivim()
 }
 
 void Minivim::run(){
-    printw("Hello World!\n");
-    getch();
+    while(mode != 'q'){
+        update();
+        statusLine();
+        int c = getch();
+        input(c);
+    }
+}
+
+void Minivim::update()
+{
+    switch (mode)
+    {
+    case 27:    
+    case 'n':
+        status = "NORMAL";
+        break;
+    case 'i':
+        status = "INSERT";
+        break;    
+    case 'q':
+        break;
+    };
+}
+
+void Minivim::statusLine()
+{
+    attron(A_REVERSE);
+    mvprintw(LINES - 1, 0, status.c_str());
+    attroff(A_REVERSE);
+}
+
+void Minivim::input(int c)
+{
+    switch (mode)
+    {
+    case 27:    
+    case 'n':
+        switch (c)
+        {
+        case 27:
+        case 'n':
+            mode = 'n';
+            break;
+        case 'i':
+            mode = 'i';
+            break;
+        }
+        break;
+    case 'i':
+        switch (c)
+        {
+        case 27:
+        case 'n':
+            mode = 'n';
+            break;
+        default:
+            std::string s(1, c);
+            lines.push_back(s);
+        }
+        break;   
+    case 'q':
+        break;
+    };
+
+    for(size_t i {}; i < lines.size(); ++i){
+        mvprintw(0, i, lines[i].c_str());
+    }
 }
--- src/minivim.hpp
+++ src/minivim.hpp
@@ -2,14 +2,23 @@
 #pragma clang diagnostic ignored "Wformat-secutity"
 #include <ncurses.h>
 #include <memory>
+#include <vector>
 #include "arquivos.hpp"
 
 class Minivim {
 private:
     FileManager *gArquivos;
     std::fstream *file;
+    std::vector<std::string> lines;
+    int x, y;
+    char mode;
+    std::string status;
 public:
     Minivim(std::string&);
     ~Minivim();
     void run();
+protected:
+    void update();
+    void statusLine();
+    void input(int);
 };
